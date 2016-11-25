8a5b308ae5b410416e471864f3004daf6d06d8a0
Roman Krasavtsev <mr.krasavtsev@gmail.com>
2016-11-25 11:15:38 +0300

    Create MySQL.sql
    add MySQL

---
+++ MySQL.sql
@@ -0,0 +1,33 @@
+# users and privileges
+select * from information_schema.user_privileges;
+
+# PROCESSLIST
+SHOW FULL PROCESSLIST
+
+# current queries
+SELECT 
+  id,
+  state,
+  command,
+  time,
+  info
+FROM information_schema.processlist
+WHERE command <> 'Sleep' 
+AND info NOT LIKE '%PROCESSLIST%'
+ORDER BY time DESC LIMIT 50;
+
+# Size of databases
+SELECT
+  table_schema as "db",
+  Round(Sum(data_length + index_length) / 1024 / 1024, 1) as "size, MB" 
+FROM information_schema.tables 
+GROUP BY table_schema
+ORDER by 2 DESC;
+
+# Size of tables
+SELECT
+  table_schema as 'db', 
+  table_name as 'table', 
+  round(((data_length + index_length) / 1024 / 1024), 2) 'size, MB' 
+FROM information_schema.TABLES 
+ORDER BY 3 DESC;
