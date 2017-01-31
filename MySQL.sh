b3fe57ac15ab807407ca172d819fd91c66334ff8
Roman Krasavtsev <mr.krasavtsev@gmail.com>
2017-01-31 13:44:07 +0300

    Update MySQL.sh

--- MySQL.sh
+++ MySQL.sh
@@ -1,3 +1,6 @@
+# Dump
 mysqldump -u username -p DB --ignore-table=db.table | gzip > `date +/backup/%Y%m%d_%H%M%S_db.sql.gz`
 
- 
+# User and password from config
+mysql_config_editor set --login-path=local --host=localhost --user=localuser --password
+mysql --login-path=local -e "statement"
